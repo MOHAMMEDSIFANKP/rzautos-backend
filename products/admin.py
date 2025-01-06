7ce81e5e4bfd6882f53e811c3777d5319299829c
mohammedsifankp <sifan007sifu@gmail.com>
2025-01-06 14:51:36 +0530

    some changes

--- products/admin.py
+++ products/admin.py
@@ -3,6 +3,12 @@ from django.http import HttpResponse
 from django.contrib import admin
 from .models import *
 from utils.helper import generate_pdf
+from django.forms.widgets import FileInput
+from django import forms
+from django.utils.html import format_html
+from django.core.exceptions import ValidationError
+import imghdr
+
 # Register your models here.
 
 @admin.register(Transmission)
@@ -26,19 +32,135 @@ class CompanyAdmin(admin.ModelAdmin):
     list_filter = ('date_added', 'is_hide')
     search_fields = ('company_name',)
 
+class MultipleFileInput(FileInput):
+    allow_multiple_selected = True
+
+class MultipleImageField(forms.FileField):
+    def __init__(self, *args, **kwargs):
+        kwargs.setdefault("widget", MultipleFileInput())
+        super().__init__(*args, **kwargs)
+
+    def validate_image_file(self, file):
+        valid_image_types = ['jpeg', 'jpg', 'png', 'gif']
+        file_type = imghdr.what(file)
+        
+        if file_type not in valid_image_types:
+            raise ValidationError(
+                f'Unsupported image format. Please use one of: {", ".join(valid_image_types)}',
+                code='invalid_image'
+            )
+
+    def clean(self, data, initial=None):
+        single_file_clean = super().clean
+        if isinstance(data, (list, tuple)):
+            result = []
+            for d in data:
+                cleaned_file = single_file_clean(d, initial)
+                if cleaned_file:
+                    self.validate_image_file(cleaned_file)
+                    result.append(cleaned_file)
+            return result
+        else:
+            result = single_file_clean(data, initial)
+            if result:
+                self.validate_image_file(result)
+            return result
+
+class CarImagesInline(admin.TabularInline):
+    model = CarImages
+    extra = 0
+    fields = ('image', 'image_alt', 'get_image_preview')
+    readonly_fields = ('get_image_preview',)
+    
+    def get_image_preview(self, obj):
+        if obj.image:
+            return format_html('<img src="{}" style="max-height: 100px;"/>', obj.image.url)
+        return "No Image"
+    get_image_preview.short_description = 'Preview'
+
+class CarsAdminForm(forms.ModelForm):
+    multiple_images = MultipleImageField(
+        required=False, 
+        help_text="Hold Ctrl/Cmd to select multiple images at once. Supported formats: JPG, JPEG, PNG, GIF"
+    )
+
+    class Meta:
+        model = Cars
+        fields = '__all__'
+
 @admin.register(Cars)
 class CarsAdmin(admin.ModelAdmin):
-    list_display = ('vehicle_registration','make','model','engine_size','transmission','fuel_type','selling_price')
-    list_filter = ('make__company_name','transmission__transmission','fuel_type__fuel_type','color__color','date_added', 'is_hide')
-    search_fields = ('vehicle_registration','model')
+    form = CarsAdminForm
+    inlines = [CarImagesInline]
+    
+    list_display = ('vehicle_registration', 'make', 'model', 'engine_size', 
+                   'transmission', 'fuel_type', 'selling_price', 'get_thumbnail_preview')
+    list_filter = ('make__company_name', 'transmission__transmission', 
+                  'fuel_type__fuel_type', 'color__color', 'date_added', 'is_hide')
+    search_fields = ('vehicle_registration', 'model')
     prepopulated_fields = {"slug": ("vehicle_registration",)}
+
+    fieldsets = (
+        ('Basic Information', {
+            'fields': ('make', 'model', 'vehicle_type', 'vehicle_registration', 'thumbnail')
+        }),
+        ('Vehicle Details', {
+            'fields': ('transmission', 'fuel_type', 'engine_size', 'color', 'body_type',
+                      'number_of_doors', 'number_of_keys', 'number_of_owners')
+        }),
+        ('Technical Information', {
+            'fields': ('chassis_number', 'mileage', 'co2_emissions')
+        }),
+        ('Sales Information', {
+            'fields': ('selling_price', 'sale_price', 'vat_type', 'sale_date')
+        }),
+        ('Documentation', {
+            'fields': ('log_book', 'purchase_invoice_number', 'registration_date')
+        }),
+        ('Description and SEO', {
+            'fields': ('description', 'slug')
+        }),
+        ('Additional Images', {
+            'fields': ('multiple_images',),
+            'description': 'Upload multiple images at once. These will be added to the existing images below.'
+        }),
+    )
+
+    def get_thumbnail_preview(self, obj):
+        if obj.thumbnail:
+            return format_html('<img src="{}" style="max-height: 50px;"/>', obj.thumbnail.url)
+        return "No Thumbnail"
+    get_thumbnail_preview.short_description = 'Thumbnail'
+
+    def save_model(self, request, obj, form, change):
+        super().save_model(request, obj, form, change)
+        
+        if 'multiple_images' in request.FILES:
+            files = request.FILES.getlist('multiple_images')
+            for image in files:
+                CarImages.objects.create(
+                    car=obj,
+                    image=image,
+                    image_alt=f"{obj.make} {obj.model} - Additional Image"
+                )
 
 @admin.register(CarImages)
 class CarImagesAdmin(admin.ModelAdmin):
-    list_display = ('image_alt',)
-    list_filter = ('car__vehicle_registration','car__make__company_name','date_added', 'is_hide')
-    search_fields = ('car__model','car__make__company_name')
-
+    list_display = ('get_car_info', 'get_image_preview', 'image_alt')
+    list_filter = ('car__vehicle_registration', 'car__make__company_name', 
+                  'date_added', 'is_hide')
+    search_fields = ('car__model', 'car__make__company_name', 'image_alt')
+    
+    def get_car_info(self, obj):
+        return f"{obj.car.make} {obj.car.model} ({obj.car.vehicle_registration})"
+    get_car_info.short_description = 'Car'
+    
+    def get_image_preview(self, obj):
+        if obj.image:
+            return format_html('<img src="{}" style="max-height: 50px;"/>', obj.image.url)
+        return "No Image"
+    get_image_preview.short_description = 'Preview'
+    
 @admin.register(Expense)
 class ExpenseAdmin(admin.ModelAdmin):
     list_display = ('car','car_expense_amount','company_expense_amount')
